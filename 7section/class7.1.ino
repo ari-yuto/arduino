3a0b80c2d0a2ab791b0cc8262651000f540e20f6
ari <ari44.0709@gmail.com>
2024-07-09 15:44:09 +0900

    7section program

---
+++ 7section/class7.1.ino
@@ -0,0 +1,39 @@
+#include <SoftwareSerial.h>
+#include "ArduinoESPAT.h"
+
+ESPAT espat("Buffalo-G-6D27", "ncu78f6a83u7u");
+
+void setup() {
+  Serial.begin(9600);
+
+  // pinMode( 13, OUTPUT);
+  if(espat.begin()){
+    Serial.println("Initialize OK");
+  } else{
+    Serial.println("Initialize Fail");
+  }
+
+  if(espat.changeMode(1)){
+    Serial.println("Mode OK");
+  } else{
+    Serial.println("Mode not OK");
+  }
+
+  if(espat.tryConnectAP()){
+    Serial.println("connected");
+  }else{
+    Serial.println("Connect Failed");
+  }
+
+  Serial.println(espat.clientIP());
+
+  Serial.println(espat.get("gihyo.jp","/book/",80));
+
+}
+
+void loop() {
+  // digitalWrite(13,HIGH);
+  // delay(100);
+  // digitalWrite(13,LOW);
+  // delay(300);
+}
