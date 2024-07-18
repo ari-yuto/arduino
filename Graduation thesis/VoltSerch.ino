4533d8b9345be1f50a6cf80219b3b00a02cb08c0
ari <ari44.0709@gmail.com>
2024-07-18 23:40:49 +0900

    serch volt

---
+++ Graduation thesis/VoltSerch.ino
@@ -0,0 +1,33 @@
+const int VOL_PIN = A0;
+
+void setup()                    
+{
+   Serial.begin(9600);
+   pinMode(12, OUTPUT);
+}
+
+void loop()                    
+{
+
+    int value;
+    float volt;
+
+
+    // Serial.print(millis() - start);        // check on performance in milliseconds
+    // Serial.print("\t");                    // tab character for debug windown spacing
+
+    // Serial.print(total1);                  // print sensor output 1
+    // Serial.println("\t");
+
+    value = analogRead( VOL_PIN );
+
+    volt = value * 5000.0 / 1023.0;
+
+    Serial.print( "Value: " );
+    Serial.print( value );
+    Serial.print( "  Volt: " );
+    Serial.print( volt );
+    Serial.println("mV");
+
+    delay(100);                             // arbitrary delay to limit data to serial port 
+}
