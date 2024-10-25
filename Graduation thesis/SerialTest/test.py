c6b6106bae8c1ed60649d35a0b0d33d138e5cd6b
ari <ari44.0709@gmail.com>
2024-10-25 14:56:17 +0900

    i can out put data

---
+++ Graduation thesis/SerialTest/test.py
@@ -0,0 +1,52 @@
+import json
+import serial
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+ser = serial.Serial('/dev/tty.usbmodem1301', 9600)
+
+# # リアルタイムでプロットするためのリスト
+# x_data = []
+# y_data = []
+
+# # 図の設定
+# fig, ax = plt.subplots()
+# line, = ax.plot([], [], lw=2)
+
+# def init():
+#     ax.set_xlim(0, 100)  # x軸の範囲
+#     ax.set_ylim(0, 1024)  # y軸の範囲（アナログ値の場合）
+#     return line,
+
+# def update(frame):
+#     # data = ser.readline().decode().strip()  # Arduinoからデータを読み込む
+#     val_arduino = ser.readline()
+#     data = pd.json_normalize(eval(val_arduino.decode()))
+#     print(data)
+#     y_data.append(int(data))
+#     x_data.append(len(y_data))  # 自動的にx軸のデータを更新
+#     line.set_data(x_data, y_data)
+#     ax.set_xlim(0, len(y_data))  # x軸を更新
+#     return line,
+
+while True:
+    val_arduino = ser.readline()
+    # text = json.loads(val_arduino.decode())
+    # print(text)
+    data = pd.json_normalize(eval(val_arduino.decode()))
+    Time = data['time']
+    press = data['DO']
+
+    print(data)
+
+
+    # plt.plot(Time, press)
+    # plt.pause()
+
+ani = FuncAnimation(fig, update, init_func=init, blit=True)
+plt.show()
+
+
+
+
