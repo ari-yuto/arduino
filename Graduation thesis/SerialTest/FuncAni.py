7987249c10f02aaa1cf897d4c147708a5648f3f7
ari <ari44.0709@gmail.com>
2024-11-06 18:29:32 +0900

    add gyro value but not finish

--- Graduation thesis/SerialTest/FuncAni.py
+++ Graduation thesis/SerialTest/FuncAni.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-ser = serial.Serial('/dev/tty.usbmodem1301', 9600)
+ser = serial.Serial('/dev/tty.usbmodem11301', 9600)
 
 # リアルタイムでプロットするためのリスト
 
@@ -14,10 +14,15 @@ y_data_press = []
 x_data_CapSen = []
 y_data_CapSen = []
 
-x_data_time = []
+x_data_time_accel = []
 X_data_accel = []
 Y_data_accel = []
 Z_data_accel = []
+
+x_data_time_gyro = []
+X_data_gyro = []
+Y_data_gyro = []
+Z_data_gyro = []
 
 # 図の設定
 fig_press, ax_press = plt.subplots()
@@ -31,6 +36,11 @@ line_accel_X, = ax_accel.plot([], [], lw=2, label='X')
 line_accel_Y, = ax_accel.plot([], [], lw=2, label='Y')
 line_accel_Z, = ax_accel.plot([], [], lw=2, label='Z')
 
+fig_gyro, ax_gyro = plt.subplots()
+line_gyro_X, = ax_gyro.plot([], [], lw=2, label='X')
+line_gyro_Y, = ax_gyro.plot([], [], lw=2, label='Y')
+line_gyro_Z, = ax_gyro.plot([], [], lw=2, label='Z')
+
 
 #　初期化関数
 def init():
@@ -38,12 +48,15 @@ def init():
     ax_press.set_ylim(0, 1024)  # y軸の範囲（アナログ値の場合）
 
     ax_CapSen.set_xlim(0, 100)  # x軸の範囲
-    ax_CapSen.set_ylim(0, 1000)  # y軸の範囲（アナログ値の場合）
+    ax_CapSen.set_ylim(0, 100)  # y軸の範囲（アナログ値の場合）
 
     ax_accel.set_xlim(0, 100)  # x軸の範囲
     ax_accel.set_ylim(-1.5, 1.5)  # y軸の範囲（アナログ値の場合）
 
-    return line_press, line_CapSen, line_accel_X, line_accel_Y, line_accel_Z
+    ax_gyro.set_xlim(0, 100)  # x軸の範囲
+    ax_gyro.set_ylim(-10, 10)  # y軸の範囲（アナログ値の場合）
+
+    return line_press, line_CapSen, line_accel_X, line_accel_Y, line_accel_Z, line_gyro_X, line_gyro_Y, line_gyro_Z
 
 def update_press(frame):
     # data = ser.readline().decode().strip()  # Arduinoからデータを読み込む
@@ -65,6 +78,7 @@ def update_CapSen(frame):
     y_data_CapSen.append(float(data['D1']))
     x_data_CapSen.append(int(data['time']))  # 自動的にx軸のデータを更新
     line_CapSen.set_data(x_data_CapSen, y_data_CapSen)
+    print(data['D1'])
     if int(data['time']) > 100:
         ax_CapSen.set_xlim(int(data['time'])-100,int(data['time']) )  # x軸を更新  
     else:
@@ -75,13 +89,13 @@ def update_CapSen(frame):
 def update_accel(frame):
     val_arduino = ser.readline()
     data = pd.json_normalize(eval(val_arduino.decode()))
-    X_data_accel.append(float(data['X']))
-    Y_data_accel.append(float(data['Y']))
-    Z_data_accel.append(float(data['Z']))
-    x_data_time.append(int(data['time']))  # 自動的にx軸のデータを更新
-    line_accel_X.set_data(x_data_time, X_data_accel)
-    line_accel_Y.set_data(x_data_time, Y_data_accel)
-    line_accel_Z.set_data(x_data_time, Z_data_accel)
+    X_data_accel.append(float(data['X_accel']))
+    Y_data_accel.append(float(data['Y_accel']))
+    Z_data_accel.append(float(data['Z_accel']))
+    x_data_time_accel.append(int(data['time']))  # 自動的にx軸のデータを更新
+    line_accel_X.set_data(x_data_time_accel, X_data_accel)
+    line_accel_Y.set_data(x_data_time_accel, Y_data_accel)
+    line_accel_Z.set_data(x_data_time_accel, Z_data_accel)
     if int(data['time']) > 100:
         ax_accel.set_xlim(int(data['time'])-100,int(data['time']) )  # x軸を更新  
     else:
@@ -89,10 +103,28 @@ def update_accel(frame):
     
     return line_accel_X, line_accel_Y, line_accel_Z
 
+def update_gyro(frame):
+    val_arduino = ser.readline()
+    data = pd.json_normalize(eval(val_arduino.decode()))
+    X_data_gyro.append(float(data['X_gyro']))
+    Y_data_gyro.append(float(data['Y_gyro']))
+    Z_data_gyro.append(float(data['Z_gyro']))
+    x_data_time_gyro.append(int(data['time']))  # 自動的にx軸のデータを更新
+    line_gyro_X.set_data(x_data_time_gyro, X_data_gyro)
+    line_gyro_Y.set_data(x_data_time_gyro, Y_data_gyro) 
+    line_gyro_Z.set_data(x_data_time_gyro, Z_data_gyro)
+    if int(data['time']) > 100:
+        ax_gyro.set_xlim(int(data['time'])-100,int(data['time']) )  # x軸を更新  
+    else:
+        ax_gyro.set_xlim(0, int(data['time']))  # x軸を更新
+    
+    return line_gyro_X, line_gyro_Y, line_gyro_Z
+
 
 ani_press = FuncAnimation(fig_press, update_press, init_func=init, blit=True , interval=53)
 ani_CapSen = FuncAnimation(fig_CapSen, update_CapSen, init_func=init, blit=True , interval=100)
-ani_accel = FuncAnimation(fig_CapSen, update_accel, init_func=init, blit=True , interval=53)
+ani_accel = FuncAnimation(fig_accel, update_accel, init_func=init, blit=True , interval=53)
+ani_gyro = FuncAnimation(fig_gyro, update_gyro, init_func=init, blit=True , interval=53)
 
 plt.legend()
 
