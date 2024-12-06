b27a8fd7e601c4aef437a3525d873f84221595e7
ari <ari44.0709@gmail.com>
2024-12-06 18:55:31 +0900

    make fft and excel file

---
+++ Graduation thesis/SerialTest/FFT.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import FuncAni
+import openpyxl
+import datetime
+
+time = datetime.datetime.now().strftime('%m-%d-%H-%M-%S')
+
+excel_path = rf"/Users/ariyoshiyuto/GitHub/arduino/Graduation thesis/SerialTest/{time}.xlsx"
+
+# フーリエ変換
+fft_data_x = np.abs(np.fft.rfft(FuncAni.X_data_accel)).tolist()  # 絶対値
+fft_data_y = np.abs(np.fft.rfft(FuncAni.Y_data_accel)).tolist()  # 絶対値
+fft_data_z = np.abs(np.fft.rfft(FuncAni.Z_data_accel)).tolist()  # 絶対値
+freqList = np.fft.rfftfreq(len(FuncAni.X_data_accel), 1.0 / 67).tolist()  # 横軸
+
+fft_datas = [fft_data_x, fft_data_y, fft_data_z]
+
+wb = openpyxl.Workbook()
+sheet = wb.worksheets[0]
+
+for row in fft_datas :
+    sheet.append(row)
+
+wb.save(excel_path)
+wb.close()
+
+plt.subplot(3, 1, 1)
+plt.plot(freqList, fft_data_x, color='blue')
+plt.legend(['X'])
+
+plt.subplot(3, 1, 2)
+plt.plot(freqList, fft_data_y, color='orange')
+plt.legend(['Y'])
+
+plt.subplot(3, 1, 3)
+plt.plot(freqList, fft_data_z, color='green')
+plt.legend(['Z'])
+
+plt.xlabel('Frequency')
+plt.ylabel('Power')
+
+plt.tight_layout()
+
+plt.show()
+
